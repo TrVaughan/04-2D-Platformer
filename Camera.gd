92a075b5bef2db6cc956606662ff175762066c42
TrVaughan <trvaugha@iu.edu>
2021-04-02 15:43:26 -0400

    m

--- Camera.gd
+++ Camera.gd
@@ -1,9 +1,9 @@
 extends Camera2D
 
-var player = get_node_or_null("/root/Game/Player_Container/Player")
+var player = get_node_or_null("/root/Game/Player_Container/PlayerScene")
 
 func _process(_delta):
 	if player == null:
-		player = get_node_or_null("/root/Game/Player_Container/Player")
+		player = get_node_or_null("/root/Game/Player_Container/PlayerScene")
 	else: 
 		position = player.position 
--- Player/Player_Container-script.gd
+++ Player/Player_Container-script.gd
@@ -1,3 +1,12 @@
 extends Node2D
 
-onready var Player = load("")
+onready var PlayerScene = load("res://Player/Player.tscn")
+var initial_position = Vector2(200,200)
+
+func _process(_delta):
+	if not has_node("PlayerScene"):
+		var Player_Instance = PlayerScene.instance()
+		Player_Instance.position = initial_position
+		Player_Instance.name = "PlayerScene"
+		add_child(Player_Instance)
+		
